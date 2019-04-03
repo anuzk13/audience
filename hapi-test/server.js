89c0978ff4994fe0ab86929c77fcd7a6a50dcc30
Carlo Sugatan <carlosugatan@gmail.com>
2019-04-03 08:24:10 -0400

    uploading hapi test

---
+++ hapi-test/server.js
@@ -0,0 +1,49 @@
+const Hapi = require('hapi')
+const Inert = require('inert')
+const fs = require('fs')
+const server = Hapi.Server({ port: 2345 })
+const init = async () => {
+  await server.register(Inert)
+
+  const handleFileUpload = file => {
+    return new Promise((resolve, reject) => {
+      const filename = file.hapi.filename
+      const data = file._data
+      fs.writeFile('./upload/' + filename, data, err => {
+        if (err) {
+          reject(err)
+        }
+        resolve({ message: 'Upload successfully!' })
+      })
+    })
+  }
+
+  server.route({
+    method: 'GET',
+    path: '/upload/{file*}',
+    handler: {
+      directory: {
+        path: 'upload'
+      }
+    }
+  })
+
+  server.route({
+    path: '/upload',
+    method: 'POST',
+    options: {
+      payload: {
+        output: 'stream',
+      }
+    },
+    handler: async (req, h) => {
+      const { payload } = req
+      const response = handleFileUpload(payload.file)
+      return response
+    }
+  })
+
+  await server.start()
+}
+
+init()
