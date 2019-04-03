5e37653a0c65156234b0275d9e9184ed287e7b25
Carlo Sugatan <carlosugatan@gmail.com>
2019-04-03 10:30:39 -0400

    updated server.js

--- hapi-test/server.js
+++ hapi-test/server.js
@@ -1,49 +1,23 @@
 const Hapi = require('hapi')
 const Inert = require('inert')
-const fs = require('fs')
 const server = Hapi.Server({ port: 2345 })
 const init = async () => {
   await server.register(Inert)
-
-  const handleFileUpload = file => {
-    return new Promise((resolve, reject) => {
-      const filename = file.hapi.filename
-      const data = file._data
-      fs.writeFile('./upload/' + filename, data, err => {
-        if (err) {
-          reject(err)
-        }
-        resolve({ message: 'Upload successfully!' })
-      })
-    })
-  }
-
   server.route({
+    path: '/',
     method: 'GET',
-    path: '/upload/{file*}',
-    handler: {
-      directory: {
-        path: 'upload'
-      }
-    }
+    handler: (req, h) => ({ message: 'Hello Hapi.js' })
   })
-
-  server.route({
-    path: '/upload',
-    method: 'POST',
-    options: {
-      payload: {
-        output: 'stream',
-      }
-    },
-    handler: async (req, h) => {
-      const { payload } = req
-      const response = handleFileUpload(payload.file)
-      return response
-    }
-  })
-
   await server.start()
 }
+
+server.route({
+  path: '/upload',
+  method: 'POST',
+  handler: (req, h) => {
+    const { payload } = req
+    return payload
+  }
+})
 
 init()
