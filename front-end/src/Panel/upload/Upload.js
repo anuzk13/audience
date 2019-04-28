c6cf21de65e53a5f20775216eb163bc0ef384817
Ana María Cárdenas Gasca <am.cardenas927@gmail.com>
2019-04-28 00:44:44 -0400

    Modify upload component

--- front-end/src/Panel/upload/Upload.js
+++ front-end/src/Panel/upload/Upload.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './Upload.css';
-import Buttons from './Buttons';
 
 class Upload extends Component {
 
@@ -9,6 +8,7 @@ class Upload extends Component {
         // this.Authentication = new Authentication()
         //if the extension is running on twitch or dev rig, set the shorthand here. otherwise, set to null. 
         this.twitch = window.Twitch ? window.Twitch.ext : null
+        this.state = {message:'Select a File'};
     }
 
 
@@ -39,10 +39,53 @@ class Upload extends Component {
             'multipart/form-data').then( a => console.log(a))
 
       }
+      getUploadedFileName = (e) => {
+        const errs = [] 
+        const files = Array.from(e.target.files)
+    
+        const formData = new FormData()
+        const types = ['image/png', 'image/jpeg', 'image/gif']
+    
+        const file = files[0];
+        
+        if (types.every(type => file.type !== type)) {
+            const message = `'${file.type}' is not a supported format`
+            this.setState({...this.state,message});
+        }
+
+        if (file.size > 400000) {
+            const message = `'${file.name}' is too large, please pick a smaller file`
+            this.setState({...this.state,message});
+        } else {
+            const message = `Uploading${file.name}`
+            formData.append('file', file)
+            this.setState({...this.state,message});
+            this.props.auth.makeCall(
+                'upload',
+                'POST',
+                formData,
+                'multipart/form-data').then( a => {
+                    const message = 'Select a File'
+                    formData.append('file', file)
+                    this.setState({...this.state,message});
+                })
+        }
+        
+     }
 
     render() {
+        const id = 'form-input'
+        const multiple = false
         return (
-            <Buttons onChange={this.onChange} />
+            <div className="file-upload">
+            <input id={id} type="file" className="km-btn-file"
+                multiple={multiple} 
+                onChange={this.getUploadedFileName}>
+            </input>
+            <label htmlFor={id} className="km-button km-button--primary km-btn-file-label">
+                <span>{this.state.message}</span>
+            </label>
+            </div>
         )
     }
 }
